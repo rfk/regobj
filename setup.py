d0816d30a8fca9f225f67bf7e815bbbb81f2e042
Ryan Kelly <ryan@rfk.id.au>
2009-08-07 15:28:46 +1000

    allow setup.py to run on non-win32 machines

--- setup.py
+++ setup.py
@@ -7,12 +7,19 @@
 
 from distutils.core import setup
 
-import regobj
-VERSION = regobj.__version__
+#  If we did a straight `import regobj` here we wouldn't be able
+#  to build on non-win32 machines.
+regobj = {}
+try:
+    execfile("regobj.py",regobj)
+except ImportError:
+    pass
+
+VERSION = regobj["__version__"]
 
 NAME = "regobj"
 DESCRIPTION = "Pythonic object-based access to the Windows Registry."
-LONG_DESC = regobj.__doc__
+LONG_DESC = regobj["__doc__"]
 AUTHOR = "Ryan Kelly"
 AUTHOR_EMAIL = "ryan@rfk.id.au"
 URL="http://www.rfk.id.au/software/"
