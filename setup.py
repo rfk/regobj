94d46c7bda9f7c81b4231add6ca8111b6203bb25
Ryan Kelly <ryan@rfk.id.au>
2011-03-24 09:58:38 +1100

    fix setup.py for python3 compatability

--- setup.py
+++ setup.py
@@ -7,11 +7,29 @@
 
 from distutils.core import setup
 
+#  Safely extract the docstring and version info from the module.
 #  If we did a straight `import regobj` here we wouldn't be able
 #  to build on non-win32 machines.
+try:
+    next = next
+except NameError:
+    def next(i):
+        return i.next()
 regobj = {}
 try:
-    execfile("regobj.py",regobj)
+    src = open("regobj.py")
+    try:
+        lines = []
+        ln = next(src)
+        while "__version__" not in ln:
+            lines.append(ln)
+            ln = next(src)
+        while "__ver" in ln:
+            lines.append(ln)
+            ln = next(src)
+        exec("".join(lines),regobj)
+    finally:
+        src.close()
 except ImportError:
     pass
 VERSION = regobj["__version__"]
